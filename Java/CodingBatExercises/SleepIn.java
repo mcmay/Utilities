79523d121b9eddff873dc61b2df306acf243c067
mcmay <mcmay@126.com>
2019-01-22 08:56:28 +1100

    first commit

---
+++ Java/CodingBatExercises/SleepIn.java
@@ -0,0 +1,12 @@
+/*The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
+
+sleepIn(false, false) → true
+sleepIn(true, false) → false
+sleepIn(false, true) → true
+*/
+
+public class SleepIn {
+	public static void main (String[] args) {
+		
+	}
+}
