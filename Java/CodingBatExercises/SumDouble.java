5cfc3d30e8d218c00d368f6064cbab69b2d6729b
mcmay <mcmay@126.com>
2019-01-24 16:57:57 +1100

    completed

--- Java/CodingBatExercises/SumDouble.java
+++ Java/CodingBatExercises/SumDouble.java
@@ -3,6 +3,22 @@ Given two int values, return their sum. Unless the two values are the same, then
 
 
 sumDouble(1, 2) → 3
-sumDouble(3, 2) → 5
+(3, 2) → 5
 sumDouble(2, 2) → 8
 */
+public class SumDouble {
+    public static int sumDouble(int a,  int b) {
+        return a == b? (a + b) : (a + b) * 2;
+    }
+    
+   public static void main (String[] args) {
+       int sum;
+       
+        sum = sumDouble(1, 2);
+        System.out.println(sum);
+        sum = sumDouble(3, 2);
+        System.out.println(sum);
+        sum = sumDouble(2, 2);
+        System.out.println(sum);
+    }
+}
