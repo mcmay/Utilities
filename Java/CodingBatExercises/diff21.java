b33eb4efdf0b8b47216c5dedc76918d122ccf2d3
mcmay <mcmay@126.com>
2019-01-25 16:55:44 +1100

    first commit

--- Java/CodingBatExercises/diff21.java
+++ Java/CodingBatExercises/diff21.java
@@ -6,4 +6,26 @@ diff21(10) → 11
 diff21(21) → 0
 */
 
-public
+public class Diff21 {
+    public static int diff21 (int n) {
+        int diff = n - 21;
+        
+        if (n < 21)
+            diff = -diff;
+        else
+            diff *= 2;
+        
+        return diff;
+    }
+    
+    public static void main (String[] args) {
+        int result;
+        
+        result = diff21(19);
+        System.out.println(result);
+        result = diff21(10);
+        System.out.println(result);
+        result = diff21(21);
+        System.out.println(result);
+    }
+}
