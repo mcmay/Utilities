dc4dbd37880d93c412c1273630d5978fb3f14e9e
mcmay <mcmay@126.com>
2019-01-28 11:58:20 +1100

    first commit

---
+++ Java/Tests/AddHomemadeObjectsToArrayList/AddHomeMadeObjectsToArrayList.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class MyObject {
+    private String str;
+
+    public MyObject(String s) {
+            this.str = s;
+    }
+    public void setStr(String str) {
+            this.str = str;
+    }
+    public String getStr() {
+            return str;
+    }
+}
+
+public class AddHomeMadeObjectsToArrayList {
+        public static void main (String[] args) {
+                String[] strings = {"Java is an object-oriented programming language.",
+                                    "Everything in Java lives in a class.",
+                                    "Java programms (bytecodes) run in the Java Virtual Machine (JVM).",
+                                    "Ok, enough for this time. See you later."
+                };
+                List <MyObject>  myObList = new ArrayList<MyObject>();
+		List <String> strList = new ArrayList<String>();
+                for (int i = 0; i < strings.length; i++) {
+			// Every time there must be a "new" MyObject instance mo 
+			// created to hold a different string
+                	MyObject mo = new MyObject("noString");
+			strList.add(strings[i]);
+                        mo.setStr(strings[i]);
+                        myObList.add(mo);
+                }
+		String dispStr = null;
+                for (int i = 0; i < myObList.size(); i++) {
+			dispStr = myObList.get(i).getStr();
+                        System.out.println(dispStr);
+                }
+                for (int i = 0; i < strList.size(); i++) {
+			dispStr = strList.get(i);
+                        System.out.println(dispStr);
+                }
+        }
+}
