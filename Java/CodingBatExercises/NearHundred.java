74d73a2012024564240443a0b0a9e3748c2a5339
mcmay <mcmay@126.com>
2019-02-01 09:12:55 +1100

    final

--- Java/CodingBatExercises/NearHundred.java
+++ Java/CodingBatExercises/NearHundred.java
@@ -1,12 +1,12 @@
 
 public class NearHundred {
-    public static boolean nearHundred {
+    public static boolean nearHundred (int n){
         int value1 = Math.abs(n - 100);
         int value2 = Math.abs(n - 200);
-        return (0 < value1 && value1 < 10) || (0 < value2 && value2 < 10) 
+        return (0 < value1 && value1 < 10) || (0 < value2 && value2 < 10);
     }
     public static void main (String[] args) {
-        int result;
+        boolean result;
         
         result = nearHundred(193);
         System.out.println(result);
