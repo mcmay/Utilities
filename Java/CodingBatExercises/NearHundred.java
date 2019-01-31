5521c9a28dcb0f6d0e6e5780e0abc9356d9022ce
mcmay <mcmay@126.com>
2019-02-01 09:06:30 +1100

    first commit

---
+++ Java/CodingBatExercises/NearHundred.java
@@ -0,0 +1,8 @@
+
+public class NearHundred {
+    public static boolean nearHundred {
+        int value1 = Math.abs(n - 100);
+        int value2 = Math.abs(n - 200);
+        return (0 < value1 && value1 < 10) || (0 < value2 && value2 < 10) 
+    }
+}
