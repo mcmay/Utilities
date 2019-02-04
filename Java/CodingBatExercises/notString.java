8de4631caea4e97686d3433a74064f6c971f9454
mcmay <mcmay@126.com>
2019-02-05 09:02:07 +1100

    first commit

---
+++ Java/CodingBatExercises/notString.java
@@ -0,0 +1,16 @@
+/*
+Given a string, return a new string where "not " has been added to the front. However, if the string already begins with "not", return the string unchanged. Note: use .equals() to compare 2 strings.
+
+
+notString("candy") → "not candy"
+notString("x") → "not x"
+notString("not bad") → "not bad"
+*/
+
+public class NotString {
+    static String notString (String str) {
+        if (str.startWith("not"))
+            return str;
+        return "not " + str;
+    }
+}
