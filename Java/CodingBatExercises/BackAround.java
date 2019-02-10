7b670139b4fa520e6793f96523f23400f06a9ad7
mcmay <mcmay@126.com>
2019-02-11 08:51:13 +1100

    first commit

---
+++ Java/CodingBatExercises/BackAround.java
@@ -0,0 +1,15 @@
+/*
+Given a string, take the last char and return a new string with the last char added at the front and back, so "cat" yields "tcatt". The original string will be length 1 or more.
+
+backAround("cat") → "tcatt"
+backAround("Hello") → "oHelloo"
+backAround("a") 
+*/
+
+public class BackAround {
+    static String backAround {
+        System.out.println ("Enter a word:");
+        Scanner scan = next();
+        
+    }
+}
