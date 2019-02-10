351e923c79c93018629d0fa9e02d9f6404741129
mcmay <mcmay@126.com>
2019-02-11 09:21:20 +1100

    updated

--- Java/CodingBatExercises/BackAround.java
+++ Java/CodingBatExercises/BackAround.java
@@ -6,19 +6,26 @@ backAround("Hello") → "oHelloo"
 backAround("a") 
 */
 import java.util.*;
-public class BackAround {
-    static String backAround (Strinf scan) {
+
+public class Main {
+    static String backAround (String scan) {
         String back = Character.toString(scan.charAt(scan.length- 1));
         return back + scan + back;
     }
     
     public static void main(String[] args) {
+		String result = null;
+	
         System.out.println ("Enter a word:");
-        Scanner s = next();
-        if (s.length);
-            String result = backAround(s);
-        else
+        Scanner s = new Scanner(System.in);
+		    s.next();
+        if (s.length) {
+			result = backAround(s);
+			System.out.println(s);
+		}
+        else {
             System.err.println("Invalid string.");
             System.exit(1);
+		}
     }
 }
